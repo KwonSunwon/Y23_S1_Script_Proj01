949612f3127494882a8b429b0c471c05710c231d
KwonSunwon <kso00011147@gmail.com>
2023-04-19 02:37:11 +0900

    작업 단위로 함수화

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ from selenium.common.exceptions import (
     NoSuchElementException,
 )
 
+# GUI
+from tkinter import *
+from tkinter.ttk import *
+
 # Crawling을 통해 획득한 문자열을 정규식으로 통해 과목이름 별로 분리
 import re
 
 # 분리한 과목이름 리스트를 통해 과목별 폴더 생성
 import os
+
+# GUI
+window = Tk()
+window.title("NewSemesterFolderCreator")
 
 # 크롬 브라우저를 띄우지 않고 크롤링
 options = webdriver.ChromeOptions()
@@ -76,3 +84,87 @@ print(subjects)
 os.mkdir(title.group())
 for subject in subjects:
     os.mkdir(title.group() + "/" + subject)
+
+
+def generate_gui():
+    window = Tk()
+    window.title("NewSemesterFolderCreator")
+
+    # 가져온 과목 체크박스 리스트
+    ui_subjectAndReadFrame = Frame(window)
+    ui_subjectAndReadFrame.pack()
+
+
+def ui_generate_subjectLists(frame, subjectList=None):
+    if subjectList == None:
+        return
+
+    subjectCheckBoxes = []
+    for subject in subjectList:
+        subjectCheckBoxes.append(Checkbutton(frame, text=subject))
+
+
+def web_generate_webdriver():
+    # 크롬 브라우저를 띄우지 않고 크롤링
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    driver = webdriver.Chrome("./chromedriver.exe", options=options)
+    url = "https://eclass.tukorea.ac.kr/ilos/main/member/login_form.acl"
+    driver.get(url)
+    return driver
+
+
+def web_login_eClass():
+    while True:
+        userID = str(input("ID: "))
+        userPW = str(input("PW: "))
+        try:
+            driver.find_element(By.NAME, "usr_id").send_keys(userID)
+            driver.find_element(By.NAME, "usr_pwd").send_keys(userPW)
+            driver.find_element(By.ID, "login_btn").click()
+            if url != driver.current_url:
+                break
+        except UnexpectedAlertPresentException as error:
+            alert = None
+            try:
+                alert = driver.switch_to.alert
+            except NoAlertPresentException:
+                pass
+            # alert.accept()
+            print("아이디와 비밀번호를 다시 확인해주세요.")
+        except (NoAlertPresentException, NoSuchElementException) as error:
+            print("아이디와 비밀번호를 다시 확인해주세요.")
+
+        # 오류가 발생하면 빠져나가서 경고 메시지 출력하도록 변경
+
+
+def web_get_subject_list():
+    driver.find_element(
+        By.CSS_SELECTOR, "#quick-menu-index > a:nth-child(1) > div"
+    ).click()
+    main = BeautifulSoup(driver.page_source, "html.parser")
+    subjectList = main.select("#lecture_list > div:nth-child(2) > div:nth-child(1)")
+    subjectList = [subject.text for subject in subjectList]
+    return subjectList[0]
+
+
+def find_semester_and_subjects(subjectStr):
+    titleRegex = re.compile(r"[0-9]{4}-[1-2]{1}학기")
+    subjectRegex = re.compile(r"\b(\S+(?:\s\S+)*)\([0-9]+\)")
+
+    title = titleRegex.search(subjectStr)
+    subjects = subjectRegex.findall(subjectStr)
+
+    return title.group(), subjects
+
+
+def set_root_folder():
+    # tk로 폴더 선택
+    # 해당 폴더로 이동
+    pass
+
+
+def make_folder(semester, subjects):
+    os.mkdir(semester)
+    for subject in subjects:
+        os.mkdir(semester + "/" + subject)
